afbe5aa2098734ecd7e5a1348de5c8d3f4929be3
雷文涛 <leiwentao@leiwentaodeMacBook-Air.local>
2022-08-25 10:05:00 +0800

    java3y auto save

--- autosave.sh
+++ autosave.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /Users/leiwentao/Library/Mobile Documents/com~apple~CloudDocs/netify/
+cd /Users/leiwentao/Library/Mobile\ Documents/com~apple~CloudDocs/netify/
 git add .
 git commit -m "java3y auto save"
 git push origin main
